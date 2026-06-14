030af57a6732dc095a8145d0caa65d10e7e46741
P-KAVYASRI <kavyasri369369@gmail.com>
2026-06-14 19:21:09 +0530

    Add Streamify backend and frontend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,62 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+
+export async function signup(req,res){
+   const {email,password,fullName}=req.body;
+   console.log(req.body);
+
+   try{
+    if(!email || !password || !fullName){
+        return res.status(400).json({message: "All fields are required"});
+    }
+    if(password.length < 6){
+        return res.status(400).json({message: "Password must be at least 6 characters long"});
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+ if (!emailRegex.test(email)) {
+  return res.status(400).json({ message: "Invalid email format" });
+}
+const existingUser = await User.findOne({email});
+if(existingUser){
+    return res.status(400).json({message: "Email already in used, please choose another one"});
+}
+
+
+const idx = Math.floor(Math.random() * 100) + 1;
+const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+const newUser = await User.create({
+    email,
+    fullName,
+    password,
+    profilePicture: randomAvatar,
+});
+
+//TODO: create the user in stream as well
+
+const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+});
+
+res.cookie("jwt",token,{
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, //prevent XSS attacks,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production", //only send cookie over HTTPS in production
+})
+
+res.status(201).json({success:true,user: newUser});
+   } catch(error){
+    console.log("Error in signup controller:", error);
+    res.status(500).json({message: "Internal server error"});
+   }
+}
+
+export async function login(req,res){
+    res.send("Login Route");
+}
+
+export async function logout(req,res){
+    res.send("Logout Route");
+}   
